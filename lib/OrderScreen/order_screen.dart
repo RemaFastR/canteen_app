a7371d3b2f0e47d10907dbb2b8dd766303e5dcee
Remik Matevosyan <rembimat@gmail.com>
2020-09-25 16:51:02 +0300

    add widget

--- lib/OrderScreen/order_screen.dart
+++ lib/OrderScreen/order_screen.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class OrderScreen extends StatefulWidget {
+  OrderScreen({Key key}) : super(key: key);
+
+  @override
+  _OrderScreenState createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('dfd'),
+    );
+  }
+}
