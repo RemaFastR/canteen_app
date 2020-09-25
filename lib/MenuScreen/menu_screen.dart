f13447baed2bbb085e82aeb3110a77e1583b6c0d
Remik Matevosyan <rembimat@gmail.com>
2020-09-25 16:58:47 +0300

    add widget

--- lib/MenuScreen/menu_screen.dart
+++ lib/MenuScreen/menu_screen.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class MenuScreen extends StatefulWidget {
+  MenuScreen({Key key}) : super(key: key);
+
+  @override
+  _MenuScreenState createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+}
